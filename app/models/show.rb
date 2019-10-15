b827f66d5e769b5dd08d86968ba67a1a53498843
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-15 18:41:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,30 @@
-class Show < ActiveRecord::Base 
-  
-  def highest_rating
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
     self.maximum(:rating)
-  end 
-  
-  def self
+  end
+
+  def self.most_popular_show
+    self.order(rating: :desc).first
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self. least_popular_show
+    self.order(rating: :asc).first
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order(name: :asc)
+  end
+end
